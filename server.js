12fc56bf90a8e19ffdb09a4c6a2a41e15b1f66c3
Mykhailo Golota <mykhailo.golota@panenco.com>
2020-12-23 23:59:23 +0200

    fix: fix

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const appPaths = require('./config/paths');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use('/ui', express.static(uiPaths.storybook));
-
 app.use(express.static(appPaths.outputPath));
 app.get('/*', (_, res) => res.sendFile(path.join(appPaths.outputPath, 'index.html')));
 
